012af7fde596e69fb274795309a205747d82bd35
wdl <1458883958@qq.com>
2019-04-19 15:42:28 +0800

    底部导航栏的制作以及页面切换

--- lib/page/index_page.dart
+++ lib/page/index_page.dart
@@ -1,14 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'home_page.dart';
+import 'assort_page.dart';
+import 'cart_page.dart';
+import 'plus_page.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  // 所有的item
+  final List<BottomNavigationBarItem> _bottomBarItems = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('会员中心'),
+    )
+  ];
+
+  // 主页对应的四个页面
+  List<Widget> _pages = [];
+
+  // 当前页面的下标
+  int _currentIndex = 0;
+
+  // 当前页
+  var _currentPage;
+
+  @override
+  void initState() {
+    _pages
+      ..add(HomePage())
+      ..add(AssortPage())
+      ..add(CartPage())
+      ..add(PlusPage());
+    _currentPage = _pages[_currentIndex];
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('百姓生活+'),
-      ),
-      body: Center(
-        child: Text('主体部分'),
+      // 设置当前页面的主体
+      body: _currentPage,
+      bottomNavigationBar: BottomNavigationBar(
+        // 固定位置，3个以上itemBar禁止动画
+        type: BottomNavigationBarType.fixed,
+        // 所有ItemBar
+        items: _bottomBarItems,
+        // 当前下标
+        currentIndex: _currentIndex,
+        // 点击切换
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            _currentPage = _pages[_currentIndex];
+          });
+        },
       ),
     );
   }
