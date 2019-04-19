89f929e092ce44f96f9c351b1dafc90e483a295a
wdl <1458883958@qq.com>
2019-04-19 16:03:26 +0800

    引入Dio

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    _getHttp();
     return Scaffold(
       body: Center(
         child: Text('HomePage'),
       ),
     );
+  }
+
+  void _getHttp() async {
+    try {
+      Response response = await Dio().get(
+          'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian');
+      return print(response);
+    } catch (e) {
+      return print(e);
+    }
   }
 }
