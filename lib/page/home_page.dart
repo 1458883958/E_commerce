3a84c73f4601af2916abfaa0f1a0ce0bc2315cfe
wdl <1458883958@qq.com>
2019-05-08 16:14:16 +0800

    Dio以及StatefulWidget使用，SingleChildScrollView

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,15 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _controller = TextEditingController();
+  var showToast = '正在等待！';
+
   @override
   Widget build(BuildContext context) {
-    _getHttp();
-    return Scaffold(
-      body: Center(
-        child: Text('HomePage'),
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('post小案例'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                autofocus: false,
+                controller: _controller,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(5.0),
+                  labelText: '类型',
+                  helperText: '请输入类型',
+                ),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  if (_controller.text == '') {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('输入类型为空'),
+                          );
+                        });
+                  } else {
+                    _postHttp(_controller.text).then((value) {
+                      setState(() {
+                        showToast = value['data']['name'];
+                      });
+                    });
+                  }
+                },
+                child: Text('点击获取服务'),
+              ),
+              Center(
+                child: Text(
+                  showToast,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Future _postHttp(String name) async {
+    print('开始获取...........');
+    var data = {'name': name};
+    try {
+      Response response = await Dio().post(
+          'https://www.easy-mock.com/mock/5cd2871821b32328e69012d6/example/post_test',
+          queryParameters: data);
+      print('${response.data}');
+      return response.data;
+    } catch (e) {
+      return print(e.toString());
+    }
   }
 
   void _getHttp() async {
